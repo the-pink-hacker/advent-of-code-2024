9c228cd95182a941027a127d36ea24bd189fa5a0
Pink Garrett <pink@thepinkhacker.com>
2024-12-02 03:44:58 -0500

    Day 2 part 2

--- days/day-2/src/main.rs
+++ days/day-2/src/main.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 const INPUT: &str = include_str!("../input");
 
 fn parse_reports(data: &str) -> Vec<Vec<u8>> {
@@ -12,6 +14,19 @@ fn parse_reports(data: &str) -> Vec<Vec<u8>> {
         .collect()
 }
 
+fn is_level_safe(level: u8, next_level: u8, report_delta: Ordering) -> bool {
+    let delta = level.cmp(&next_level);
+
+    // Change in direction
+    if delta != report_delta {
+        return false;
+    }
+
+    let distance = level.abs_diff(next_level);
+
+    matches!(distance, 1..=3)
+}
+
 fn is_report_safe(report: &[u8]) -> bool {
     assert!(report.len() >= 2);
 
@@ -21,32 +36,42 @@ fn is_report_safe(report: &[u8]) -> bool {
         let level = report[i];
         let next_level = report[i + 1];
 
-        let distance = level.abs_diff(next_level);
-        let delta = level.cmp(&next_level);
-
-        // Change in direction
-        if delta != report_delta {
+        if !is_level_safe(level, next_level, report_delta) {
             return false;
-        }
-
-        match distance {
-            1..=3 => (),
-            _ => return false,
         }
     }
 
     true
 }
 
-fn part_one(reports: &[Vec<u8>]) {
-    let total_safe = reports
+fn is_report_mostly_safe(report: &[u8]) -> bool {
+    if is_report_safe(report) {
+        true
+    } else {
+        for i in 0..report.len() {
+            let mut report = report.to_vec();
+            report.remove(i);
+            if is_report_safe(&report) {
+                return true;
+            }
+        }
+
+        false
+    }
+}
+
+fn part_one(reports: &[Vec<u8>]) -> usize {
+    reports
         .iter()
         .filter(|report| is_report_safe(report))
-        .count();
+        .count()
+}
 
-    println!();
-    println!("Part 1");
-    println!("Safe Reports: {}", total_safe);
+fn part_two(reports: &[Vec<u8>]) -> usize {
+    reports
+        .iter()
+        .filter(|report| is_report_mostly_safe(report))
+        .count()
 }
 
 fn main() {
@@ -54,7 +79,17 @@ fn main() {
 
     println!("=== Day 2 ===");
 
-    part_one(&reports);
+    let total_safe = part_one(&reports);
+
+    println!();
+    println!("Part 1");
+    println!("Safe Reports: {}", total_safe);
+
+    let total_mostly_safe = part_two(&reports);
+
+    println!();
+    println!("Part 2");
+    println!("Mostly Safe Reports: {}", total_mostly_safe);
 }
 
 #[cfg(test)]
@@ -95,5 +130,59 @@ mod test {
     fn example_safe_6() {
         let report = vec![1, 3, 6, 7, 9];
         assert!(is_report_safe(&report));
+    }
+
+    #[test]
+    fn example_mostly_safe_1() {
+        let report = vec![7, 6, 4, 2, 1];
+        assert!(is_report_mostly_safe(&report));
+    }
+
+    #[test]
+    fn example_mostly_safe_2() {
+        let report = vec![1, 2, 7, 8, 9];
+        assert!(!is_report_mostly_safe(&report));
+    }
+
+    #[test]
+    fn example_mostly_safe_3() {
+        let report = vec![9, 7, 6, 2, 1];
+        assert!(!is_report_mostly_safe(&report));
+    }
+
+    #[test]
+    fn example_mostly_safe_4() {
+        let report = vec![1, 3, 2, 4, 5];
+        assert!(is_report_mostly_safe(&report));
+    }
+
+    #[test]
+    fn example_mostly_safe_5() {
+        let report = vec![8, 6, 4, 4, 1];
+        assert!(is_report_mostly_safe(&report));
+    }
+
+    #[test]
+    fn example_mostly_safe_6() {
+        let report = vec![1, 3, 6, 7, 9];
+        assert!(is_report_mostly_safe(&report));
+    }
+
+    #[test]
+    fn exmaple_mostly_safe_first_fail() {
+        let report = vec![10, 9, 11, 12, 13, 14];
+        assert!(is_report_mostly_safe(&report));
+    }
+
+    #[test]
+    fn part_one_final() {
+        let reports = parse_reports(INPUT);
+        assert_eq!(part_one(&reports), 287);
+    }
+
+    #[test]
+    fn part_two_final() {
+        let reports = parse_reports(INPUT);
+        assert_eq!(part_two(&reports), 354);
     }
 }
